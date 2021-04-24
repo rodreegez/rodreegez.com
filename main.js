7868226953aaa1ec4e1790e9903ee72c9b2c0f07
Adam <electronicbattleweapon7@gmail.com>
2021-04-24 21:56:46 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+import './style.css'
+
+async function fetchPosts() {
+  const response = await fetch('https://dev.to/api/articles?username=rodreegez');
+  const posts = await response.json();  
+  return posts;
+}
+
+const postTemplate = (post) => `
+  <li>
+    <a href="${post.url}">
+      <h2>${ post.title }</h2>
+      <p class="text-sm text-gray-800">${ post.description }</p>
+    </a>
+
+  </li>
+`
+
+const writingContainer = document.querySelector('#recent-posts');
+
+fetchPosts().then(posts => {
+  for (var post of posts) {
+    console.log(post);
+    writingContainer.insertAdjacentHTML('beforeend', postTemplate(post));
+  }
+})
