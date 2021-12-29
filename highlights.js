2aeffc0377049ab23cc68d769ded508c51ad6ae5
Adam <electronicbattleweapon7@gmail.com>
2021-12-29 14:08:44 +0000

    click highlight to refresh

--- highlights.js
+++ highlights.js
@@ -1,6 +1,6 @@
 import highlights from './highlights.json'
 
-let allHighlights = []
+const allHighlights = []
 
 for(let highlight of highlights) {
   for(let quote of highlight.highlights) {
@@ -8,6 +8,15 @@ for(let highlight of highlights) {
   }
 }
 
-let randomHighlight = allHighlights[Math.floor(Math.random()*allHighlights.length)]
+const randomHighlight = () => {
+  return allHighlights[Math.floor(Math.random()*allHighlights.length)]
+}
 
-document.querySelector('#random-highlight').innerText = randomHighlight
+const refreshHighlight = () => {
+  document.querySelector('#random-highlight').innerText = randomHighlight()
+}
+
+window.refreshHighlight = refreshHighlight
+
+refreshHighlight()
+
