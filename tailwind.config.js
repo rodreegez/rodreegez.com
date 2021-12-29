4b54fd3b30de1b55b92fee4b24dc513ef04fc9be
Adam <electronicbattleweapon7@gmail.com>
2021-12-29 14:08:29 +0000

    upgrade all the things

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./index.html', './*.{vue,js,ts,jsx,tsx}'],
+  content: ['./index.html', './*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
